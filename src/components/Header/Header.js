04a59dc037031777ca9ed319faa5f41f2914b79d
vstromatias <stromatiasvasilis@gmail.com>
2023-11-01 17:47:59 +0200

    ...

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Logo from "../../assets/moneygram-logo.png";
+
+function Header() {
+  return (
+    <div>
+      Header
+      <img src={Logo} alt="Logo" />
+    </div>
+  );
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// COMPONENTS
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <Home />
+    <Footer />
   </React.StrictMode>
 );
 
